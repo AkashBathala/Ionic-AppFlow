85cf36f963273406d67d7590ed8d48f0c1816fb2
Akash Bathla <akash.bathla2013@gmail.com>
2019-08-02 14:50:39 +0530

    Some color chages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Deploy } from 'plugins/cordova-plugin-ionic/dist/ngx';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,17 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private deploy: Deploy
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
+    this.platform.ready().then(async () => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      await this.deploy.configure({ channel: 'BETA' });
     });
   }
 }
