509f154b2b26cd73839a6005d5f86c6d0c259eda
Akash Bathla <akash.bathla2013@gmail.com>
2019-08-06 18:03:35 +0530

    New changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Deploy } from 'plugins/cordova-plugin-ionic/dist/ngx';
+// import { Deploy } from 'plugins/cordova-plugin-ionic/dist/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -14,7 +14,7 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private deploy: Deploy
+    // private deploy: Deploy
   ) {
     this.initializeApp();
   }
@@ -25,7 +25,7 @@ export class AppComponent {
       this.statusBar.styleLightContent();
       this.statusBar.backgroundColorByHexString('#701337');
       this.splashScreen.hide();
-      await this.deploy.configure({ channel: 'BETA' });
+      // await this.deploy.configure({ channel: 'BETA' });
     });
   }
 }
