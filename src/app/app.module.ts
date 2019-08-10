f8ab1cc121ef577d7731740d1845708ba3ef8a0a
Akash Bathla <akash.bathla2013@gmail.com>
2019-08-10 18:09:19 +0530

    29934

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddVisitorPageComponent } from './add-visitor-page/add-visitor-page.component';
-// import { Deploy } from 'plugins/cordova-plugin-ionic/dist/ngx';
 
 @NgModule({
-  declarations: [AppComponent, AddVisitorPageComponent],
-  entryComponents: [],
+  declarations: [
+    AppComponent,
+    AddVisitorPageComponent
+  ],
+  entryComponents: [
+    AddVisitorPageComponent
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
-    // Deploy,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
